77a1ffdf3c32dbf672785cc442294cc45bd604b6
Marco Aurelio Rezende <marco.aurelio.rsilva7@gmail.com>
2025-07-28 14:57:48 -0300

    feat: add checkbox terms to signup form

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -10,6 +10,7 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import { Checkbox } from '@/components/ui/checkbox'
 import { Input } from '@/components/ui/input'
 
 const SignupPage = () => {
@@ -26,6 +27,20 @@ const SignupPage = () => {
           <Input placeholder="Digite seu email" />
           <PasswordInput />
           <PasswordInput placeholder="Confirme sua senha" />
+          <div className="items-top flex space-x-2">
+            <Checkbox id="terms" />
+            <div className="grid gap-1.5 leading-none">
+              <label
+                htmlFor="terms"
+                className="text-xs text-muted-foreground opacity-75"
+              >
+                Ao clicar em "Criar conta", você aceita{' '}
+                <a href="#" className="text-white underline">
+                  nosso termo de uso e política de privacidade.
+                </a>
+              </label>
+            </div>
+          </div>
         </CardContent>
         <CardFooter>
           <Button className="w-full">Criar conta</Button>
